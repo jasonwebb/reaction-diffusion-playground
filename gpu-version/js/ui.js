76d8d8c2fb8920f4034759241bdadd7b945317a6
Jason Webb <zen.webb@gmail.com>
2020-06-27 21:16:13 -0500

    Normalizing code styling

--- gpu-version/js/ui.js
+++ gpu-version/js/ui.js
@@ -36,49 +36,59 @@ function setupReactionDiffusionParameters() {
     }
   });
 
+  // f ------------------------------------------
   pane.addInput(parameterValues, 'f', {
     label: 'f',
     min: parameterMetadata.f.min,
     max: parameterMetadata.f.max,
     step: .0001
-  }).on('change', (value) => {
-    simulationUniforms.f.value = value;
-  });
+  })
+    .on('change', (value) => {
+      simulationUniforms.f.value = value;
+    });
 
+  // k ------------------------------------------
   pane.addInput(parameterValues, 'k', {
     label: 'k',
     min: parameterMetadata.k.min,
     max: parameterMetadata.k.max,
     step: .0001
-  }).on('change', (value) => {
-    simulationUniforms.k.value = value;
-  });
+  })
+    .on('change', (value) => {
+      simulationUniforms.k.value = value;
+    });
 
+  // dA -----------------------------------------
   pane.addInput(parameterValues, 'dA', {
     label: 'dA',
     min: parameterMetadata.dA.min,
     max: parameterMetadata.dA.max,
     step: .0001
-  }).on('change', (value) => {
-    simulationUniforms.dA.value = value;
-  });
+  }
+    ).on('change', (value) => {
+      simulationUniforms.dA.value = value;
+    });
 
+  // dB -----------------------------------------
   pane.addInput(parameterValues, 'dB', {
     label: 'dB',
     min: parameterMetadata.dB.min,
     max: parameterMetadata.dB.max,
     step: .0001
-  }).on('change', (value) => {
-    simulationUniforms.dB.value = value;
-  });
+  })
+    .on('change', (value) => {
+      simulationUniforms.dB.value = value;
+    });
 
+  // Timestep -----------------------------------
   pane.addInput(parameterValues, 'timestep', {
     label: 'Timestep',
     min: parameterMetadata.timestep.min,
     max: parameterMetadata.timestep.max
-  }).on('change', (value) => {
-    simulationUniforms.timestep.value = value;
-  });
+  })
+    .on('change', (value) => {
+      simulationUniforms.timestep.value = value;
+    });
 }
 
 
@@ -96,17 +106,19 @@ function setupSeedFolder() {
       Text: InitialTextureTypes.TEXT,
       Image: InitialTextureTypes.IMAGE,
     }
-  }).on('change', (value) => {
-    currentSeedType = parseInt(value);
-    pane.dispose();
-    setupUI();
-  });
+  })
+    .on('change', (value) => {
+      currentSeedType = parseInt(value);
+      pane.dispose();
+      setupUI();
+    });
 
   seedFolder.addButton({
-    title: 'Restart with this pattern'
-  }).on('click', () => {
-    drawFirstFrame(currentSeedType);
-  });
+    title: '⟳ Restart with this pattern'
+  })
+    .on('click', () => {
+      drawFirstFrame(currentSeedType);
+    });
 }
 
 
@@ -127,11 +139,12 @@ function setupRenderingFolder() {
       'Black and white': 5,
       'Raw': 6
     }
-  }).on('change', (value) => {
-    displayUniforms.renderingStyle.value = value;
-    pane.dispose();
-    setupUI();
-  });
+  })
+    .on('change', (value) => {
+      displayUniforms.renderingStyle.value = value;
+      pane.dispose();
+      setupUI();
+    });
 
   renderingFolder.addSeparator();
 
@@ -170,106 +183,106 @@ function setupRenderingFolder() {
     function addGradientOptions(folder) {
       // Color 1 --------------------------------------------
       folder.addInput(parameterValues.gradientColors, 'color1RGB', { label: 'Color 1' })
-      .on('change', (value) => {
-        displayUniforms.colorStop1.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop1.value.w);
-      });
+        .on('change', (value) => {
+          displayUniforms.colorStop1.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop1.value.w);
+        });
 
       folder.addInput(parameterValues.gradientColors, 'color1Stop', { label: 'Threshold', min: 0.0, max: 1.0 })
-      .on('change', (value) => { displayUniforms.colorStop1.value.w = value; });
+        .on('change', (value) => { displayUniforms.colorStop1.value.w = value; });
 
       folder.addInput(parameterValues.gradientColors, 'color1Enabled', { label: 'Enabled' })
-      .on('change', (checked) => {
-        if(checked) {
-          displayUniforms.colorStop1.value = parameterValues.lastColorStop1;
-        } else {
-          parameterValues.lastColorStop1 = displayUniforms.colorStop1.value;
-          displayUniforms.colorStop1.value = new THREE.Vector4(-1, -1, -1, -1);
-        }
-      });
+        .on('change', (checked) => {
+          if(checked) {
+            displayUniforms.colorStop1.value = parameterValues.lastColorStop1;
+          } else {
+            parameterValues.lastColorStop1 = displayUniforms.colorStop1.value;
+            displayUniforms.colorStop1.value = new THREE.Vector4(-1, -1, -1, -1);
+          }
+        });
 
       folder.addSeparator();
 
       // Color 2 --------------------------------------------
       folder.addInput(parameterValues.gradientColors, 'color2RGB', { label: 'Color 2' })
-      .on('change', (value) => {
-        displayUniforms.colorStop2.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop2.value.w);
-      });
+        .on('change', (value) => {
+          displayUniforms.colorStop2.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop2.value.w);
+        });
 
       folder.addInput(parameterValues.gradientColors, 'color2Stop', { label: 'Threshold', min: 0.0, max: 1.0 })
-      .on('change', (value) => { displayUniforms.colorStop2.value.w = value; });
+        .on('change', (value) => { displayUniforms.colorStop2.value.w = value; });
 
       folder.addInput(parameterValues.gradientColors, 'color2Enabled', { label: 'Enabled' })
-      .on('change', (checked) => {
-        if(checked) {
-          displayUniforms.colorStop2.value = parameterValues.lastColorStop2;
-        } else {
-          parameterValues.lastColorStop2 = displayUniforms.colorStop2.value;
-          displayUniforms.colorStop2.value = new THREE.Vector4(-1, -1, -1, -1);
-        }
-      });
+        .on('change', (checked) => {
+          if(checked) {
+            displayUniforms.colorStop2.value = parameterValues.lastColorStop2;
+          } else {
+            parameterValues.lastColorStop2 = displayUniforms.colorStop2.value;
+            displayUniforms.colorStop2.value = new THREE.Vector4(-1, -1, -1, -1);
+          }
+        });
 
       folder.addSeparator();
 
       // Color 3 --------------------------------------------
       folder.addInput(parameterValues.gradientColors, 'color3RGB', { label: 'Color 3' })
-      .on('change', (value) => {
-        displayUniforms.colorStop3.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop3.value.w);
-      });
+        .on('change', (value) => {
+          displayUniforms.colorStop3.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop3.value.w);
+        });
 
       folder.addInput(parameterValues.gradientColors, 'color3Stop', { label: 'Threshold', min: 0.0, max: 1.0 })
-      .on('change', (value) => { displayUniforms.colorStop3.value.w = value; });
+        .on('change', (value) => { displayUniforms.colorStop3.value.w = value; });
 
       folder.addInput(parameterValues.gradientColors, 'color3Enabled', { label: 'Enabled' })
-      .on('change', (checked) => {
-        if(checked) {
-          displayUniforms.colorStop3.value = parameterValues.lastColorStop3;
-        } else {
-          parameterValues.lastColorStop3 = displayUniforms.colorStop3.value;
-          displayUniforms.colorStop3 = new THREE.Vector4(-1, -1, -1, -1);
-        }
-      });
+        .on('change', (checked) => {
+          if(checked) {
+            displayUniforms.colorStop3.value = parameterValues.lastColorStop3;
+          } else {
+            parameterValues.lastColorStop3 = displayUniforms.colorStop3.value;
+            displayUniforms.colorStop3.value = new THREE.Vector4(-1, -1, -1, -1);
+          }
+        });
 
       folder.addSeparator();
 
       // Color 4 --------------------------------------------
       folder.addInput(parameterValues.gradientColors, 'color4RGB', { label: 'Color 4' })
-      .on('change', (value) => {
-        displayUniforms.colorStop4.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop4.value.w);
-      });
+        .on('change', (value) => {
+          displayUniforms.colorStop4.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop4.value.w);
+        });
 
       folder.addInput(parameterValues.gradientColors, 'color4Stop', { label: 'Threshold', min: 0.0, max: 1.0 })
-      .on('change', (value) => { displayUniforms.colorStop4.value.w = value; });
+        .on('change', (value) => { displayUniforms.colorStop4.value.w = value; });
 
       folder.addInput(parameterValues.gradientColors, 'color4Enabled', { label: 'Enabled' })
-      .on('change', (checked) => {
-        if(checked) {
-          displayUniforms.colorStop4.value = parameterValues.lastColorStop4;
-        } else {
-          parameterValues.lastColorStop4 = displayUniforms.colorStop4.value;
-          displayUniforms.colorStop4.value = new THREE.Vector4(-1, -1, -1, -1);
-        }
-      });
+        .on('change', (checked) => {
+          if(checked) {
+            displayUniforms.colorStop4.value = parameterValues.lastColorStop4;
+          } else {
+            parameterValues.lastColorStop4 = displayUniforms.colorStop4.value;
+            displayUniforms.colorStop4.value = new THREE.Vector4(-1, -1, -1, -1);
+          }
+        });
 
       folder.addSeparator();
 
       // Color 5 --------------------------------------------
       folder.addInput(parameterValues.gradientColors, 'color5RGB', { label: 'Color 5' })
-      .on('change', (value) => {
-        displayUniforms.colorStop5.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop5.value.w);
-      });
+        .on('change', (value) => {
+          displayUniforms.colorStop5.value = new THREE.Vector4(value.r/255, value.g/255, value.b/255, displayUniforms.colorStop5.value.w);
+        });
 
       folder.addInput(parameterValues.gradientColors, 'color5Stop', { label: 'Threshold', min: 0.0, max: 1.0 })
-      .on('change', (value) => { displayUniforms.colorStop5.value.w = value; });
+        .on('change', (value) => { displayUniforms.colorStop5.value.w = value; });
 
       folder.addInput(parameterValues.gradientColors, 'color5Enabled', { label: 'Enabled' })
-      .on('change', (checked) => {
-        if(checked) {
-          displayUniforms.colorStop5.value = parameterValues.lastColorStop5;
-        } else {
-          parameterValues.lastColorStop5 = displayUniforms.colorStop5.value;
-          displayUniforms.colorStop5.value = new THREE.Vector4(-1, -1, -1, -1);
-        }
-      });
+        .on('change', (checked) => {
+          if(checked) {
+            displayUniforms.colorStop5.value = parameterValues.lastColorStop5;
+          } else {
+            parameterValues.lastColorStop5 = displayUniforms.colorStop5.value;
+            displayUniforms.colorStop5.value = new THREE.Vector4(-1, -1, -1, -1);
+          }
+        });
     }
 
 
@@ -319,6 +332,7 @@ function setupCanvasSize() {
       });
   }
 
+  // Maximized checkbox ---------------------------------------------------
   canvasSizeFolder.addInput(containerSize, 'isMaximized', { label: 'Maximize' })
     .on('change', (checked) => {
       if(checked) {
@@ -355,17 +369,19 @@ function setupActions() {
   const actionsFolder = pane.addFolder({ title: 'Actions' });
 
   actionsFolder.addButton({
-    title: '⏸️ Pause/play'
-  }).on('click', () => {
-    isPaused = !isPaused;
-  });
+    title: '⏸ Pause/play'
+  })
+    .on('click', () => {
+      isPaused = !isPaused;
+    });
 
   actionsFolder.addButton({
     title: '💾 Save as image'
-  }).on('click', () => {
-    let link = document.createElement('a');
-    link.download = 'reaction-diffusion.png';
-    link.href = renderer.domElement.toDataURL();
-    link.click();
-  });
+  })
+    .on('click', () => {
+      let link = document.createElement('a');
+      link.download = 'reaction-diffusion.png';
+      link.href = renderer.domElement.toDataURL();
+      link.click();
+    });
 }
