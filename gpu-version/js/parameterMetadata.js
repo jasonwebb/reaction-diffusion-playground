c0a0dc01208772ff5c5e909370659254fcddcc83
Jason Webb <zen.webb@gmail.com>
2020-07-17 19:56:33 -0500

    Updating param limits to be consistent with param map

--- gpu-version/js/parameterMetadata.js
+++ gpu-version/js/parameterMetadata.js
@@ -1,27 +1,27 @@
 export default {
   f: {
-    min: 0.01,
+    min: 0.0,
     max: 0.1,
     initial: 0.054
   },
   k: {
-    min: 0.05,
-    max: 0.1,
+    min: 0.03,
+    max: 0.07,
     initial: 0.062
   },
   dA: {
     min: 0.2,
     max: 0.25,
-    initial: 0.2097
+    initial: .2
   },
   dB: {
     min: 0.01,
     max: 0.8,
-    initial: 0.105
+    initial: .1
   },
   timestep: {
-    min: 0.01,
+    min: 0.0,
     max: 2.0,
-    initial: 2.0
+    initial: 1.0
   }
 };
