e61ef66e75a1525fe6da31bf93f4ccd6b4b1cee3
Jason Webb <zen.webb@gmail.com>
2020-07-17 19:55:27 -0500

    Correcting values of Karl Sim's Laplacian weights, removing unnecessary clamp() and adding debug trick to generate param map

--- gpu-version/glsl/simulationFrag.glsl
+++ gpu-version/glsl/simulationFrag.glsl
@@ -1,5 +1,4 @@
 /**
-
 - Red channel = concentration of chemical A (0.0 - 1.0)
 - Green channel = concentration of chemical B (0.0 - 1.0)
 */
@@ -21,9 +20,9 @@ vec3 weights[3];
 void setWeights(int type) {
   // Karl Sim's weights from http://www.karlsims.com/rd.html
   if(type == 0) {
-    weights[0] = vec3(0.2,   0.05,  0.2);
-    weights[1] = vec3(0.05, -1.0,   0.05);
-    weights[2] = vec3(0.2,   0.05,  0.2);
+    weights[0] = vec3(0.05,  0.2,  0.05);
+    weights[1] = vec3(0.2,  -1.0,  0.2);
+    weights[2] = vec3(0.05,  0.2,  0.05);
 
   // Standard (?) 9-point stencil from https://en.wikipedia.org/wiki/Discrete_Laplace_operator
   } else if(type == 1) {
@@ -31,7 +30,7 @@ void setWeights(int type) {
     weights[1] = vec3(0.5,  -3.0,  0.5);
     weights[2] = vec3(0.25,  0.5,  0.25);
 
-  // 5-point stencil
+  // 5-point stencil from Xmoprhoa/pmneila source code
   } else if(type == 2) {
     weights[0] = vec3(0.0,  1.0,  0.0);
     weights[1] = vec3(1.0, -4.0,  1.0);
@@ -76,14 +75,18 @@ void main() {
     }
   }
 
+  // DEBUGGING: override f/k uniforms to generate parameter map
+  // float f = 0.1 * v_uvs[0].y;
+  // float k = 0.03 + 0.04 * v_uvs[0].x;
+
   // Pre-calculate complex and repeated terms
   vec2 laplacian = getLaplacian(centerTexel);
   float reactionTerm = A * B * B;
 
-  gl_FragColor = clamp(vec4(
+  gl_FragColor = vec4(
     A + ((dA * laplacian[0] - reactionTerm + f * (1.0 - A)) * timestep),
     B + ((dB * laplacian[1] + reactionTerm - (k + f) * B) * timestep),
     centerTexel.b,
     1.0
-  ), 0.0, 1.0);
+  );
 }
