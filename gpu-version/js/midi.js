38de1f57fb798d9d4e4c5fc6535ce3c757b5e506
Jason Webb <zen.webb@gmail.com>
2020-10-17 14:32:08 -0500

    Refreshing the Tweakpane UI when params change from MIDI, also expanding LPD8 MIDI controls to HSL mapping params

--- gpu-version/js/midi.js
+++ gpu-version/js/midi.js
@@ -5,6 +5,9 @@
 import WebMIDI from 'webmidi';
 import parameterMetadata from './parameterMetadata';
 import { InitialTextureTypes, drawFirstFrame } from './firstFrame';
+import { simulationUniforms, displayUniforms } from './uniforms';
+import { refreshUI } from './ui';
+import parameterValues from './parameterValues';
 
 export function setupMIDI() {
   WebMIDI.enable((error) => {
@@ -47,22 +50,47 @@ export function setupMIDI() {
           // Top row = 1-4 -------------------------------------------------------------------------------------------
           case 1:
             simulationUniforms.f.value = e.value.map(0, 127, parameterMetadata.f.min, parameterMetadata.f.max);
+            parameterValues.f = simulationUniforms.f.value;
             break;
 
           case 2:
             simulationUniforms.k.value = e.value.map(0, 127, parameterMetadata.k.min, parameterMetadata.k.max);
+            parameterValues.k = simulationUniforms.k.value;
             break;
 
           case 3:
             simulationUniforms.dA.value = e.value.map(0, 127, parameterMetadata.dA.min, parameterMetadata.dA.max);
+            parameterValues.dA = simulationUniforms.dA.value;
             break;
 
           case 4:
             simulationUniforms.dB.value = e.value.map(0, 127, parameterMetadata.dB.min, parameterMetadata.dB.max);
+            parameterValues.dB = simulationUniforms.dB.value;
             break;
 
           // Bottom row = 5-8 ----------------------------------------------------------------------------------------
+          case 5:
+            displayUniforms.hslFrom.value.x = e.value.map(0, 127, 0.0, 1.0);
+            parameterValues.hsl.from.min = displayUniforms.hslFrom.value.x;
+            break;
+
+          case 6:
+            displayUniforms.hslFrom.value.y = e.value.map(0, 127, 0.0, 1.0);
+            parameterValues.hsl.from.max = displayUniforms.hslFrom.value.y;
+            break;
+
+          case 7:
+            displayUniforms.hslTo.value.x = e.value.map(0, 127, 0.0, 1.0);
+            parameterValues.hsl.to.min = displayUniforms.hslTo.value.x;
+            break;
+
+          case 8:
+            displayUniforms.hslTo.value.y = e.value.map(0, 127, 0.0, 1.0);
+            parameterValues.hsl.to.max = displayUniforms.hslTo.value.y;
+            break;
         }
+
+        refreshUI();
       });
     }
   });
