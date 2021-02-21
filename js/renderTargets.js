6814239e38962e5616dcdfaf4080e21efa8caff0
Jason Webb <zen.webb@gmail.com>
2021-02-21 13:37:14 -0600

    Switching to NearestFilter on render textures for sharper images (no pixel smoothing caused by interpolation)

--- js/renderTargets.js
+++ js/renderTargets.js
@@ -19,8 +19,8 @@ export function setupRenderTargets() {
   // Create two globally-available render targets so we can "ping pong" between them (pass the result of one as input to the other, as many times as needed) and run multiple iterations per frame
   for(let i=0; i<2; i++) {
     let nextRenderTarget = new THREE.WebGLRenderTarget(parameterValues.canvas.width, parameterValues.canvas.height, {
-      minFilter: THREE.LinearFilter,
-      magFilter: THREE.LinearFilter,
+      minFilter: THREE.NearestFilter,
+      magFilter: THREE.NearestFilter,
       format: THREE.RGBAFormat,
       type: THREE.FloatType
     });
